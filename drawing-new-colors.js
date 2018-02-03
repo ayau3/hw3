8fa425f9d70269fa4b16d6103a28acd1966d54e4
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-02-03 12:45:58 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,30 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(150, 0, 0);
+  } else if (key == 'G') {
+    fill(200, 255, 150);
+  } else if (key == 'B') {
+    fill(50, 150, 255); 
+  } else if (key == 'P') {
+    fill(100, 0, 255); 
+  }
+  else if (key == 'Y') {
+    fill(255, 255, 0); 
+  }
+  else if (key == 'O') {
+    fill(255, 150, 0); 
+  }
+}
