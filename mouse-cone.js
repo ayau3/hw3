c9b4a76a01def6769fc29c71a51a4429e970ca92
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-02-08 10:32:09 -0800

    Update mouse-cone.js

--- mouse-cone.js
+++ mouse-cone.js
@@ -1,5 +1,11 @@
-var count = 0
-while (count < width) {
-line(width/2, 0, mouseX, mouseY)
-count += 10
+function setup() { 
+  createCanvas(400, 400);
+} 
+function draw() { 
+  background(340);
+  var count = 0
+	while (count< width) {
+		line(width/2, 0, mouseX, mouseY)
+		count += 10
+	}
 }
